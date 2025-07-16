45883f0fd7576f3c7de1bc62445e8cbfd12605f6
adrian-lys-dev <drugakadrian@gmail.com>
2025-07-16 19:55:14 +0300

    footer added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { HeaderComponent } from "./layout/header/header.component";
 import { HomeComponent } from "./features/home/home.component";
+import { FooterComponent } from "./layout/footer/footer.component";
 
 @Component({
   selector: 'app-root',
-  imports: [HeaderComponent, HomeComponent],
+  imports: [HeaderComponent, HomeComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
