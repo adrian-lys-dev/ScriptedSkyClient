72d53ca5e316394b1b006b98f0cddb1c09fb849c
adrian-lys-dev <drugakadrian@gmail.com>
2025-07-16 19:40:51 +0300

    home page added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { HeaderComponent } from "./layout/header/header.component";
+import { HomeComponent } from "./features/home/home.component";
 
 @Component({
   selector: 'app-root',
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, HomeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
