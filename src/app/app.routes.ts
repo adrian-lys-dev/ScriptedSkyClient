fd754468615f0cb05ac17cf7e8e26361da7690b6
adrian-lys-dev <drugakadrian@gmail.com>
2025-07-24 18:36:25 +0300

    checkout setup added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,12 +6,14 @@ import { NotFoundComponent } from './shared/components/not-found/not-found.compo
 import { ServerErrorComponent } from './shared/components/server-error/server-error.component';
 import { BookDetailComponent } from './features/shop/book-detail/book-detail.component';
 import { CartComponent } from './features/cart/cart.component';
+import { CheckoutComponent } from './features/checkout/checkout.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'shop', component: ShopComponent },
   { path: 'shop/:id', component: BookDetailComponent },
   { path: 'cart', component: CartComponent },
+  { path: 'checkout', component: CheckoutComponent },
   { path: 'test-error', component: TestErrorComponent },
   { path: 'not-found', component: NotFoundComponent },
   { path: 'server-error', component: ServerErrorComponent },
