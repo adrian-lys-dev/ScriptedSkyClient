6400c3f13f0d47f8030436317f896378193a43d0
adrian-lys-dev <drugakadrian@gmail.com>
2025-08-18 07:30:23 +0300

    dropdown component hover added && admin order list rework

--- src/app/features/admin/orders/orders.component.ts
+++ src/app/features/admin/orders/orders.component.ts
@@ -8,6 +8,7 @@ import { MatPaginator, PageEvent } from '@angular/material/paginator';
 import { DropdownComponent } from "../../../shared/components/dropdown/dropdown.component";
 import { DropdownIcon, DropdownOption } from '../../../shared/models/dropdown/dropDownOptions';
 import { Router } from '@angular/router';
+import { SnackbarService } from '../../../core/services/snackbar.service';
 
 @Component({
   selector: 'app-orders',
@@ -19,6 +20,7 @@ export class OrdersComponent implements OnInit {
 
   private adminOrderService = inject(AdminOrderService);
   private router = inject(Router);
+  private snackbar = inject(SnackbarService);
 
   paginationParams = new PaginationParams();
   orders?: Pagination<Order>;
@@ -44,84 +46,105 @@ export class OrdersComponent implements OnInit {
   }
 
   getDropDownOptions(order: Order): DropdownOption[] {
-    return [
+
+    const options: DropdownOption[] = [
       {
         label: 'Order details',
         action: () => this.router.navigate([`/account/user-profile/order/${order.id}`]),
+        class: 'text-gray-500',
+        iconHover: 'group-hover:text-gray-900',
         icon: DropdownIcon.Eye
-      },
-      ...(order.status === 'Pending' 
-        ? [{
-            label: 'Confirm Order',
+      }
+    ];
+
+    switch (order.status) {
+      case 'Pending':
+        options.push(
+          {
+            label: 'Confirm order',
             action: () => this.confirmOrder(order),
+            class: 'text-blue-600',
+            iconHover: 'group-hover:text-blue-800',
             icon: DropdownIcon.Check
-          }]
-        : []),
-      ...(order.status === 'Confirmed'
-        ? [{
-            label: 'Mark as done',
+          },
+          {
+            label: 'Cancel order',
+            action: () => this.cancelOrder(order),
+            class: 'text-red-600',
+            icon: DropdownIcon.Trash
+          }
+        );
+        break;
+
+      case 'Confirmed':
+        options.push(
+          {
+            label: 'Complete order',
             action: () => this.doneOrder(order),
             class: 'text-green-600',
+            iconHover: 'group-hover:text-green-800',
             icon: DropdownIcon.Done
-          }]
-        : []),
-      ...(order.status === 'Pending'
-        ? [{
+          },
+          {
             label: 'Cancel order',
-            class: 'text-red-600',
             action: () => this.cancelOrder(order),
+            class: 'text-red-600',
             icon: DropdownIcon.Trash
-          }]
-        : [])
-    ];
+          }
+        );
+        break;
+
+      case 'Done':
+      case 'Cancelled':
+        break;
+    }
+
+    return options;
   }
 
   confirmOrder(order: Order) {
-    this.loadingOrders.add(order.id);
-
-    this.adminOrderService.updateOrderStatus(order.id, "Confirmed").subscribe({
-      next: () => {
-        order.status = 'Confirmed';
-        this.loadingOrders.delete(order.id);
-        this.getDropDownOptions(order);
-      },
-      error: error => {
-        console.error(`Error confirming order ${order.id}:`, error);
-        this.loadingOrders.delete(order.id);
-      }
-    });
+    this.updateOrderStatusForOrder(order, 'Confirmed');
   }
 
   doneOrder(order: Order) {
+    this.updateOrderStatusForOrder(order, 'Done');
+  }
+
+  cancelOrder(order: Order) {
+    this.updateOrderStatusForOrder(order, 'Cancelled');
+  }
+
+  private updateOrderStatusForOrder(order: Order, status: 'Confirmed' | 'Done' | 'Cancelled') {
     this.loadingOrders.add(order.id);
 
-    this.adminOrderService.updateOrderStatus(order.id, "Done").subscribe({
+    this.adminOrderService.updateOrderStatus(order.id, status).subscribe({
       next: () => {
-        order.status = 'Done';
+        order.status = status;
         this.loadingOrders.delete(order.id);
+
         this.getDropDownOptions(order);
+
+        let message = '';
+        switch (status) {
+          case 'Confirmed':
+            message = `Order #${order.id} has been successfully confirmed.`;
+            break;
+          case 'Done':
+            message = `Order #${order.id} is now marked as completed.`;
+            break;
+          case 'Cancelled':
+            message = `Order #${order.id} has been cancelled.`;
+            break;
+        }
+
+        this.snackbar.success(message);
       },
-      error: error => {
-        console.error(`Error marking order ${order.id} as done:`, error);
+      error: (error) => {
+        console.error(`Error updating order ${order.id} to ${status}:`, error);
         this.loadingOrders.delete(order.id);
+        this.snackbar.error(`Failed to update Order #${order.id}. Please try again.`);
       }
     });
   }
 
-  cancelOrder(order: Order) {
-
-    this.loadingOrders.add(order.id);
-
-    this.adminOrderService.updateOrderStatus(order.id, "Cancelled").subscribe({
-      next: () => {
-        order.status = 'Cancelled';
-        this.loadingOrders.delete(order.id);
-        this.getDropDownOptions(order);
-      },
-      error: error => {
-        console.error(`Error cancelling order ${order.id}:`, error);
-        this.loadingOrders.delete(order.id);
-      }
-    })
-  }
 }
--- src/app/shared/models/dropdown/dropDownOptions.ts
+++ src/app/shared/models/dropdown/dropDownOptions.ts
@@ -9,5 +9,6 @@ export type DropdownOption = {
   label: string;
   icon?: DropdownIcon;
   class?: string;
+  iconHover?: string;
   action: () => void;
 };
